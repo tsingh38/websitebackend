93aa0fbe07131b752d58b1ce83d3595520adf9b0
Roxy <er.tajinder89@gmail.com>
2020-01-04 16:11:28 +0100

    inital commit

---
+++ src/main/java/com/lastrada/website/lastrada/LastradaApplication.java
@@ -0,0 +1,13 @@
+package com.lastrada.website.lastrada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastradaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LastradaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lastrada/website/lastrada/LastradaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lastrada.website.lastrada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LastradaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
