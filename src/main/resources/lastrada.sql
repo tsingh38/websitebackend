e1bfb759bc8bb2923b6df69a9186c68e52f095d5
Roxy <er.tajinder89@gmail.com>
2020-01-05 12:48:15 +0100

    data model complete

---
+++ src/main/resources/lastrada.sql
@@ -0,0 +1,3 @@
+-- Create database and create user and manually Grant the CRUD access to the newly created User.
+create database website_dev;
+create user 'website_dev_user'@'localhost'   IDENTIFIED BY 'Csc@2011';
