b8715e91e9acc674f4b320ba702f46149084eeae
Roxy <er.tajinder89@gmail.com>
2020-01-07 19:58:06 +0100

    improvised

--- src/main/java/com/lastrada/website/lastrada/controller/WebsiteController.java
+++ src/main/java/com/lastrada/website/lastrada/controller/WebsiteController.java
@@ -1,12 +1,17 @@
 package com.lastrada.website.lastrada.controller;
 
 import com.lastrada.website.lastrada.model.Product;
+import com.lastrada.website.lastrada.model.ProductAddition;
+import com.lastrada.website.lastrada.model.ProductOption;
 import com.lastrada.website.lastrada.pojo.ProductPOJO;
 import com.lastrada.website.lastrada.services.ShopService;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,4 +26,23 @@ public class WebsiteController {
 	    public  List<Product>  fetchAllItems() {
 	     return shopService.fetchAllItems();
 	    }
+	   
+	   
+	   @RequestMapping(value = "/saveProduct")
+	   public ResponseEntity saveProduct(@RequestBody Product product) {
+		   shopService.saveProduct(product);
+		   return ResponseEntity.ok(HttpStatus.OK);
+	   }
+	   
+	   @RequestMapping(value = "/saveProductOption")
+	   public ResponseEntity saveProductOption(@RequestBody ProductOption productOption) {
+		   shopService.saveProductOption(productOption);
+		   return ResponseEntity.ok(HttpStatus.OK);
+	   }
+	   
+	   @RequestMapping(value = "/saveProductAddition")
+	   public ResponseEntity saveProductAddition(@RequestBody ProductAddition productAddition) {
+		   shopService.saveProductAddition(productAddition);
+		   return ResponseEntity.ok(HttpStatus.OK);
+	   }
 }
--- src/main/java/com/lastrada/website/lastrada/services/ShopService.java
+++ src/main/java/com/lastrada/website/lastrada/services/ShopService.java
@@ -7,7 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lastrada.website.lastrada.model.Product;
+import com.lastrada.website.lastrada.model.ProductAddition;
+import com.lastrada.website.lastrada.model.ProductOption;
 import com.lastrada.website.lastrada.pojo.ProductPOJO;
+import com.lastrada.website.lastrada.repository.ProductAdditionRepository;
+import com.lastrada.website.lastrada.repository.ProductOptionRepository;
 import com.lastrada.website.lastrada.repository.ProductRepository;
 
 @Service
@@ -15,10 +19,31 @@ public class ShopService {
 	@Autowired
 	private ProductRepository productRepository;
 	
+	@Autowired
+	private ProductOptionRepository productOptionRepository;
+	
+	@Autowired
+	private ProductAdditionRepository productAdditionRepository;
+	
 	public List<Product> fetchAllItems() {
 	return (List<Product>) productRepository.findAll();
 	}
 	
+	public void saveProduct(Product product) {
+		this.productRepository.save(product);
+	}
+
+	public void saveProductOption(ProductOption productOption) {
+		this.productOptionRepository.save(productOption);
+		
+	}
+	
+	public void saveProductAddition(ProductAddition productAddition) {
+		this.productAdditionRepository.save(productAddition);
+		
+	}
+	
+
 	
 	
 }
