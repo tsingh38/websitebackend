414e31d4ccfe59e9f7cc7e739c01e16133319c55
Roxy <er.tajinder89@gmail.com>
2020-01-11 10:32:06 +0100

    project setup

--- src/main/java/com/lastrada/website/lastrada/controller/WebsiteController.java
+++ src/main/java/com/lastrada/website/lastrada/controller/WebsiteController.java
@@ -11,6 +11,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +22,7 @@ public class WebsiteController {
 	@Autowired
 	private ShopService shopService;
 	
-	
+	@CrossOrigin
 	   @RequestMapping(value = "/allitems")
 	    public  List<Product>  fetchAllItems() {
 	     return shopService.fetchAllItems();
